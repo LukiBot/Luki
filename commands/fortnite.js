8f7876567cc187012f525f19a0cb68d48d230d3b
Roy <root@mrsheldon.me>
2018-08-02 01:36:29 +0300

    Update fortnite.js

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -4,8 +4,8 @@ const Discord = require('discord.js');
 
 
 exports.run = async (client, msg, args, level) => {
-  if (!['pc', 'xbl', 'psn'].includes(args[0])) return msg.channel.send('**Please Include valid platform**\n`j!fortnite pc/xbl/psn username`');
-  if (!args[1]) return msg.channel.send('**Please Include username**\n`j!fortnite pc/xbl/psn username`');
+  if (!['pc', 'xbl', 'psn'].includes(args[0])) return msg.channel.send('**Please Include valid platform**\n`o!fortnite pc/xbl/psn username`');
+  if (!args[1]) return msg.channel.send('**Please Include username**\n`o!fortnite pc/xbl/psn username`');
      var platform = args[0];
      var playerName = args[1];
      fortnite.user(playerName, platform).then(data => {
