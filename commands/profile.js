613b01e42e87bf467ffc81c537e1cc6f7eb31f18
Roy <root@mrsheldon.me>
2018-08-02 04:32:15 +0300

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -17,32 +17,71 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   } else {
       user = message.author;
   }
+let rankLevel;
+if (client.config.ldevelopers.includes(userid) === true) {
+  rankLevel = 10;
+} else if (client.config.developers.includes(userid) === true) {
+  rankLevel = 9;
+} else if (client.config.managers.includes(userid) === true) {
+  rankLevel = 8;
+} else if (client.config.hadmins.includes(userid) === true) {
+  rankLevel = 7;
+} else if (client.config.admins.includes(userid) === true) {
+  rankLevel = 6;
+} else if (client.config.hmods.includes(userid) === true) {
+  rankLevel = 5;
+} else if (client.config.mods.includes(userid) === true) {
+  rankLevel = 4
+} else if (client.config.premiump.includes(userid) === true) {
+  rankLevel = 3;
+} else if (client.config.premium.includes(userid) === true) {
+  rankLevel = 2;
+} else {
+  rankLevel = 0;
+}
+const rank = client.config.permLevels.find(l => l.level === rankLevel).name;
+
+let badges;
+if (rankLevel === 10) {
+  badges = "<:Developer:395238214778224640> <:Staff:395238244859772929> <:Trusted:395240218506166273>";
+} else if (rankLevel === 9) {
+  badges = "<:Developer:395238214778224640> <:Staff:395238244859772929> <:Trusted:395240218506166273>";
+} else if (rankLevel === 8) {
+  badges = "<:Staff:395238244859772929> <:Trusted:395240218506166273>";
+} else if (rankLevel === 7) {
+  badges = "<:Staff:395238244859772929> <:Trusted:395240218506166273>";
+} else if (rankLevel === 6) {
+  badges = "<:Staff:395238244859772929> <:Trusted:395240218506166273>";
+} else if (rankLevel === 5) {
+  badges = "<:Staff:395238244859772929> <:Trusted:395240218506166273>";
+} else if (rankLevel === 4) {
+  badges = "<:Staff:395238244859772929> <:Trusted:395240218506166273>";
+} else if (rankLevel === 3 ) {
+  badges = "<:Donator:395238233103007755> <:Trusted:395240218506166273>";
+} else if (rankLevel === 2) {
+  badges = "<:Donator:395238233103007755> <:Trusted:395240218506166273>";
+} else if (rankLevel === 1) {
+  badges = "<:Trusted:395240218506166273>";
+} else {
+  badges = "";
+}
   db.get(`SELECT * FROM users WHERE id = ?`, [userid], (err, row) => {
     if (err) return console.log (err.message);
     if (!row) {
       db.run(`INSERT INTO users(id) VALUES(?)`, [userid], function(err) {
         if (err) return console.log(err.message);
-        console.log("added user to users database")
-        db.get(`SELECT * FROM users WHERE id =?`, [userid], (err, row2) => {
-          if (err) return console.log(err.message);
-          const embed2 = new Discord.RichEmbed()
-          .setThumbnail(user.avatarURL)
-          .setTitle(`${username}'s Profile`)
-          .setDescription(row2.title)
-          .addField("Level:", row2.level, true)
-          .addField("Exp:", row2.exp, true)
-          .addField("Bio:", row2.bio, false)
-          message.channel.send({embed2});        
+        console.log(`A row has been inserted with rowid ${this.lastID}`)
           });
-          });
-          return;
+        return;
     } else {
       const embed = new Discord.RichEmbed()
       .setThumbnail(user.avatarURL)
-      .setTitle(`${username}'s Profile`)
+      .setAuthor(`${username}'s Profile`, user.avatarURL)
+      .setTitle(badges)
       .setDescription(row.title)
       .addField("Level:", row.level, true)
       .addField("Exp:", row.exp, true)
+      .addField("Rank:", rank, true)
       .addField("Bio:", row.bio, false)
       message.channel.send({embed});
      }
