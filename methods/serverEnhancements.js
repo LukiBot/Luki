889ad849fd877b4426d36bdc44450c95cb5aa6f7
Olybear9 <olybear9@gmail.com>
2018-08-05 01:05:28 -0600

    Update serverEnhancements.js
    Added `getGuildInfo(guildCollection)`

--- methods/serverEnhancements.js
+++ methods/serverEnhancements.js
@@ -1,3 +1,28 @@
+require('discord.js');
 exports.function = {
-
+ getGuildInfo: function (guild) {
+  /*
+  Returns Object
+  */
+  let members = guild.members.map(m => m.user.tag); //member tags
+  let botCount = guild.members.filter(m => m.user.bot == true).size; //bot count
+  let botAccounts = guild.members.filter(m => m.user.bot == true).map(bot => bot.user.tag); // Get bot names
+  let userCount = guild.members.size - botCount;
+  let userAccounts = guild.members.filter(m => m.user.bot == false).map(m => m.user.tag);
+  let roleCount = guild.roles.size;
+  let roleNames = guild.roles.map(role => role.name);
+  let created = guild.createdAt;
+  return {
+   members: members,
+   botUsers: botAccounts,
+   botCount: botCount,
+   memberCount: userCount,
+   memberNames: userAccounts,
+   role: {
+    names: roleNames,
+    amount: roleCount
+   } 
+  };
+   //End of getGuildInfo(guild)
+ }
 }
