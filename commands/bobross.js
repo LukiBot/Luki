e8bc95fdfe0259a5fac7c3fceca787146fc988bd
Roy <root@mrsheldon.me>
2018-08-21 09:02:16 +0300

    Create bobross.js

---
+++ commands/bobross.js
@@ -0,0 +1,26 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+
+const { MessageAttachment } = require("discord.js");
+const Idiot = require("idiotic-api");
+client.API = new Idiot.Client("", {dev: true});
+const bobRoss = async () => {
+ await message.channel.send(new MessageAttachment(
+ await client.API.bobRoss(message.mentions.users.first().displayAvatarURL({ format: "png", size: 128 })),
+ "bobRoss.png"));
+};
+bobRoss();
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "bobross",
+  category: "Canvas",
+  description: "An Idiotic Bob Ross effect",
+  usage: "bobross @user"
+};
