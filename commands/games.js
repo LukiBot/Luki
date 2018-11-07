60a2307bd51bb5ab55a9ab6afadfb7d1c2c73e96
Olybear9 <olybear9@gmail.com>
2018-11-06 19:19:31 -0700

    Create games.js

---
+++ commands/games.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, level) => { 
+    // code here
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "",
+  category: "",
+  description: "",
+  usage: ""
+};
